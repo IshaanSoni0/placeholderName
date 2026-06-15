a5c6d2588786a3e5a3b8959d66a483ae6d8ebe5c
Ishaan <ishaansoni.1535@gmail.com>
2026-06-15 12:26:17 -0700

    setup commit

---
+++ src/sample.java
@@ -0,0 +1,8 @@
+public class sample {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Commit test");
+    }
+}
+//  use: javac -d bin src/App.java -------- to compile to bin
+//  use: java -cp bin App -------- to run the App.class in bin 
